dbce01fbd2efc639072c951c51a51134e03463a5
Giulian Giger <113535103+giulian-coding@users.noreply.github.com>
2026-09-19 19:08:27 +0000

    Refactor Codebase and Initialize JWT Functionality

---
+++ cmd/main.go
@@ -0,0 +1,20 @@
+﻿package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/giulian-coding/Distribution/internal/auth"
+)
+
+func main() {
+	host := "test"
+	port := 8080
+
+	token, err := auth.CreateToken(host, port)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println(token)
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/giulian-coding/Distribution
+
+go 1.27.1
+
+require github.com/golang-jwt/jwt/v5 v5.3.1 // indirect
